1a096eacc64753aec67ce79009c6a7e181913b10
Zachary Taylor <zachary.taylor@L121-202106-01.local>
2025-06-30 13:29:57 -0400

    Finish renderer by passing props

--- layers/base/components/DynamicRenderer.vue
+++ layers/base/components/DynamicRenderer.vue
@@ -2,11 +2,11 @@
   <component
     v-for="(component, index) in componentData"
     :key="index"
-    :is="component.componentName" />
+    :is="component.componentName"
+    v-bind="{ ...component }" />
 
   <pre>
       {{ componentData }}
-
     </pre
   >
 </template>
--- layers/base/components/MagazineHero.vue
+++ layers/base/components/MagazineHero.vue
@@ -1,7 +1,29 @@
 <template>
-  <section class="magazine-hero">from magazine hero</section>
+  <section class="magazine-hero">
+    from magazine hero
+    <!-- 
+    <pre>
+        {{ props }}
+
+    </pre> -->
+  </section>
 </template>
 
-<script setup></script>
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+  },
+  subTitle: {
+    type: String,
+    default: "",
+  },
+  superTitle: {
+    type: String,
+    default: "",
+  },
+  coverImage: { type: Object, default: () => {} },
+});
+</script>
 
 <style scoped></style>

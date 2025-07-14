ef93e217a4ec185ec870949561ff3f3d9a93fba2
Zachary Taylor <zachary.taylor@MacBookPro-10.lan>
2025-07-13 22:21:49 -0400

    testing

--- layers/base/pages/[...path].vue
+++ layers/base/pages/[...path].vue
@@ -1,6 +1,6 @@
 <template>
   <main class="entry" :class="pageSlug == '/' ? 'home' : pageSlug">
-    <div v-if="pageData && pageData?.components.length">
+    <div v-if="pageData">
       <DynamicRenderer :componentData="pageData?.components" />
     </div>
     <div v-else class="">We're sorry, we couldn't load your data.</div>
@@ -11,8 +11,6 @@
 const route = useRoute();
 const config = useRuntimeConfig();
 const pageSlug = generatePageSlugFromRoute(route.path);
-// const innerRouteData = route.matched[0].components.default.__file.split("/");
-// const collection = innerRouteData[innerRouteData.length - 2];
 const pageData = ref(null);
 
 onMounted(async () => {

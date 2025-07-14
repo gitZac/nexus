d6d9eeb95e1a9d62a5c3739201479f9bd45cacf3
Zachary Taylor <zachary.taylor@MacBookPro-10.lan>
2025-07-13 21:59:17 -0400

    Update ocnfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
+const runtimeConfig = useRuntimeConfig();
+
 export default defineNuxtConfig({
-  css: [`@/layers/${process.env.DEPLOYED_STORE}/assets/css/global.css`],
-  extends: ["./layers/" + process.env.DEPLOYED_STORE, "./layers/base"],
+  css: [`@/layers/${runtimeConfig.deployedStore}/assets/css/global.css`],
+  extends: ["./layers/" + runtimeConfig.deployedStore, "./layers/base"],
   devtools: { enabled: true },
   components: {
     global: true,

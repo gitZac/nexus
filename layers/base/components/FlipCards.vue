a82b2b83820cff72a9853c6a930bb35473db5cf8
Zachary Taylor <Zachary.Taylor@Mac.lan>
2026-03-14 19:44:53 -0400

    setup

---
+++ layers/base/components/FlipCards.vue
@@ -0,0 +1,40 @@
+<template>
+  <article class="flip-cards">
+    <pre>{{ props }}</pre>
+  </article>
+</template>
+
+<script setup lang="ts">
+const getCtaRoles = (roles) => {
+  const computedRoles = [];
+
+  roles.forEach((role) => {
+    computedRoles.push(`button--is-${role.item}`);
+  });
+
+  return computedRoles;
+};
+
+const props = defineProps({
+  sectionContent: {
+    type: Object,
+    default: () => ({
+      title: "",
+      text: "",
+    }),
+  },
+  flipCard: {
+    type: Array,
+    default: () => [],
+  },
+});
+</script>
+
+<style lang="scss">
+@use "./../../../assets/scss/abstracts" as dx;
+@use "./../../../assets/scss/abstracts/mixins" as dx-mx;
+@use "./../../../assets/scss/abstracts/functions" as dx-fn;
+@use "./../../capybara/assets/scss/variables.scss" as vars;
+.flip-cards {
+}
+</style>
